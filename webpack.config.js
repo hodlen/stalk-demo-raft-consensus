0d10e80cebce13d1485eed0f9204ac53797ec335
Holden <34213478+hodlen@users.noreply.github.com>
2023-04-03 09:35:17 +0800

    chore: tune for dev

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,7 @@ module.exports = [
     entry: './src/index.tsx',
     target: 'web',
     devtool: 'source-map',
+    mode: process.env.NODE_ENV || 'development',
     module: {
       rules: [
         {
